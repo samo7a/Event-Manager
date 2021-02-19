974e58f1db146afba1f5f7668c6952c5a575abb4
StaceyD22 <stacey.dale2311@gmail.com>
2021-02-18 20:56:34 -0500

    page title component

---
+++ frontend/src/components/PageTitle.js
@@ -0,0 +1,14 @@
+import './PageTitle.scss';
+import React from 'react';
+
+const PageTitle = () => {
+    return (
+        <div className="page-title">
+            <h1>
+                EventUp
+            </h1>
+        </div>
+    )
+}
+
+export default PageTitle;
