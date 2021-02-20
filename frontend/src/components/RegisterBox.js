7c0c095d45013f6354b863ffb1266de6522d0e37
StaceyD22 <stacey.dale2311@gmail.com>
2021-02-20 11:42:03 -0500

    fixed form input issue (not formatted)

--- frontend/src/components/RegisterBox.js
+++ frontend/src/components/RegisterBox.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import PageTitle from './PageTitle';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 
 const RegisterBox = (props) => {
-    let firstName = "";
-    let lastName = "";
-    let email = "";
-    let uni = "";
-    let username = "";
-    let password = "";
-    let password2 = "";
+
+    const [fName, setfName] = useState("");
+    const [lName, setlName] = useState("");
+    const [email, setEmail] = useState("");
+    const [uni, setUni] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
 
     const registerHandler = (event) => {
         event.preventDefault();
-        console.log(firstName);
-        if(firstName !== "" && lastName != "") {
-            alert(`Registering ${firstName} ${lastName}`);
+        console.log(fName);
+        if(fName !== "" && lName != "") {
+            alert(`Registering ${fName} ${lName}`);
         } else {
             alert("Where's your data??");
         }
@@ -30,39 +31,39 @@ const RegisterBox = (props) => {
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridFirstname">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" placeholder="First name" ref={(c) => firstName = c }/>
+                            <Form.Control type="text" placeholder="First name" onChange={(event) => {setfName(event.target.value)}}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridLastName">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" placeholder="Last name" ref={(c) => lastName = c} />
+                            <Form.Control type="text" placeholder="Last name" onChange={(event) => {setlName(event.target.value)}} />
                         </Form.Group>
                     </Form.Row>
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Email address" ref={(c) => email = c }/>
+                            <Form.Control type="email" placeholder="Email address" onChange={(event) => {setEmail(event.target.value)}}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridUniversity">
                             <Form.Label>University</Form.Label>
-                            <Form.Control type="text" placeholder="My university" ref={(c) => uni = c} />
+                            <Form.Control type="text" placeholder="My university" onChange={(event) => {setUni(event.target.value)}} />
                         </Form.Group>
                     </Form.Row>
                     <Form.Row>
                         <Form.Group as={Col} controlId="formGridUsername">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Username" ref={(c) => username = c }/>
+                            <Form.Control type="text" placeholder="Username" onChange={(event) => {setUsername(event.target.value)}}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="password" placeholder="Password" ref={(c) => password = c }/>
+                            <Form.Control type="password" placeholder="Password" onChange={(event) => {setPassword(event.target.value)}}/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword2">
                             <Form.Label>Retype password</Form.Label>
-                            <Form.Control type="password" placeholder="Retype password" ref={(c) => password2 = c} />
+                            <Form.Control type="password" placeholder="Retype password" onChange={(event) => {setPassword2(event.target.value)}} />
                         </Form.Group>
                     </Form.Row>
                     <button type="submit" onClick={registerHandler}>
