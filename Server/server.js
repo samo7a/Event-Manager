b64afa18fd9884038e098aeeefc57d2322708e54
Ahmed Elshetany <samo7a98@mail.com>
2021-03-03 16:29:14 -0500

    Fixed the problem of the .env file

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fetch = require('node-fetch');
+require('dotenv').config();
 
 const mysql = require('mysql');
 const conn = mysql.createConnection({
-    host : "localhost",
-    user : "superUser",
-    password : "superPassword",
-    database : "EventManager",
+    host : process.env.DATA_BASE_HOST,
+    user : process.env.DATA_BASE_USER,
+    password : process.env.DATA_BASE_PASSWORD,
+    database : process.env.DATA_BASE_NAME,
     dialectOptions: {
         insecureAuth: true
     }
 });
 conn.connect( (err) => {
-    if (err) throw err;
-    console.log('Database connected');
+    if (err) console.log('Database error');
+    else console.log('Database connected');
 });
 
 const app = express();
@@ -58,33 +60,48 @@ app.post('/api/login', async (req, res, next) =>
     res.status(200).json(response);
 });
 
-app.post('/api/signup', async (req, res, next) => 
+app.post('/api/signup', async (req, res) => 
 {
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let password = req.body.password;
-    let email = req.body.email;
-    let radioValue = req.body.radioValue;
-    let sql;
-    if (radioValue == 0) {
-        sql = `INSERT INTO SuperAdmins (sa_firstName, sa_lastName, sa_password, sa_email)
-        VALUES (${firstName}, ${lastName}, ${password}, ${email});`;
+    // let firstName = req.body.fName;
+    // let lastName = req.body.lName;
+    // let password = req.body.password;
+    // let email = req.body.email;
+     let radioValue = req.body.radioValue;
+    // let sql;
+    // if (radioValue == 0) {
+    //     sql = `INSERT INTO SuperAdmins (sa_firstName, sa_lastName, sa_password, sa_email)
+    //     VALUES (${firstName}, ${lastName}, ${password}, ${email});`;
         
+    // }
+    // else {
+    //     sql = `INSERT INTO Students (s_firstName, s_lastName, s_password, s_email)
+    //     VALUES (${firstName}, ${lastName}, ${password}, ${email});`;
+    // }
+    // conn.query(sql, (error, result) => {
+    //     if (error) {
+    //         let response = {msg: error};
+    //         res.status(200).json(response);
+    //     }
+    //     else {
+    //         let response = {msg: "Signed Up!"};
+    //         res.status(200).json(response);
+    //     }
+    // });
+    if (radioValue == 1){
+        let url = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${process.env.GOOGLE_GEOCODE_API_KEY}`;
+        let response = await fetch(url);
+        let json = await response.json();
+        //console.log(json);
+        //res.status(200).json(json);
+        let lat = json.results[0].geometry.location.lat;
+        let lng = json.results[0].geometry.location.lng;
+        let address = json.results[0].formatted_address;
+        let result = {address : address, longitude : lng, lattitude : lat};
+        res.status(200).json(result);
+        console.log(result);    
     }
-    else {
-        sql = `INSERT INTO Students (s_firstName, s_lastName, s_password, s_email)
-        VALUES (${firstName}, ${lastName}, ${password}, ${email});`;
-    }
-    conn.query(sql, (error, result) => {
-        if (error) {
-            let response = {msg: error};
-            res.status(200).json(response);
-        }
-        else {
-            let response = {msg: "Signed Up!"};
-            res.status(200).json(response);
-        }
-    });
 });
-
-app.listen(3001);
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`listenning on port ${port}`);
+});
