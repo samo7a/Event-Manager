a59f4eb4acfa25af0035b4b31296d42aa481e461
Jonathan Frucht <jonfrucht@gmail.com>
2021-03-09 12:50:32 -0500

    Styling

--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import './SideMenu.css';
-import Sidebar from 'react-sidebar';
-import Nav from 'react-bootstrap/Nav';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import "./SideMenu.css";
+import Sidebar from "react-sidebar";
+import Nav from "react-bootstrap/Nav";
+import Col from "react-bootstrap/Col";
 
 const SideMenu = () => {
-
-    return (
-        <Col xs={2}>
-            <Nav defaultActiveKey="/Home" className="flex-column">
-                <Nav.Link href="/Home" className="my-nav-link">Home</Nav.Link>
-                <Nav.Link className="my-nav-link">My Events</Nav.Link>
-                <Nav.Link className="my-nav-link">My Groups</Nav.Link>
-                <Nav.Link className="my-nav-link">My Account</Nav.Link>
-                <Nav.Link className="my-nav-link">My Profile</Nav.Link>
-            </Nav>
-        </Col>
-    )
-}
+  return (
+    <Col style={{ paddingRight: 0 }}>
+      <Nav defaultActiveKey="/Home" className="flex-column">
+        <Nav.Link href="/Home" className="my-nav-link">
+          Home
+        </Nav.Link>
+        <Nav.Link className="my-nav-link">My Events</Nav.Link>
+        <Nav.Link className="my-nav-link">My Groups</Nav.Link>
+        <Nav.Link className="my-nav-link">My Account</Nav.Link>
+        <Nav.Link className="my-nav-link">My Profile</Nav.Link>
+      </Nav>
+    </Col>
+  );
+};
 
 export default SideMenu;
