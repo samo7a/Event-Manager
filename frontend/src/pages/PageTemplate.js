7755e9048d9132ac35e5c9eb0ae8fd8369b6fd98
Jonathan Frucht <jonfrucht@gmail.com>
2021-03-20 19:06:07 -0400

    Added css file for PageTemplate

--- frontend/src/pages/PageTemplate.js
+++ frontend/src/pages/PageTemplate.js
@@ -4,55 +4,23 @@ import Banner from "../components/Banner";
 import Container from "react-bootstrap/esm/Container";
 import { Col, Row } from "react-bootstrap";
 import SideMenu from "../components/SideMenu";
-
-const PageTemplate = () => {
+import "./PageTemplate.css";
+const PageTemplate = (props) => {
   return (
     <div
       className=" "
-      style={{ height: "100vh", backgroundColor: "green", overflow: "hidden" }}
+      // style={{ height: "100vh", backgroundColor: "green", overflow: "hidden" }}
     >
-      {/* the div wrapper will place the side bar with other elements  */}
       <Banner />
-      <Container
-        fluid
-        style={{
-          padding: "0px",
-          margin: "0",
-          height: "100%",
-          backgroundColor: "yellow",
-        }}
-      >
+      <Container fluid>
         <Row>
-          <Col
-            //   class="col-2"
-            sm={1}
-            fluid
-            style={{
-              padding: "0px",
-              margin: "0 px",
-              backgroundColor: "blue",
-            }}
-          >
+          <Col sm={1} fluid className="leftCol">
             <div>
               <SideMenu />
             </div>
           </Col>
-          <Col
-            //   class="col-10"
-            sm={11}
-            //   sm="auto"
-            //   fluid
-            style={{
-              // paddingLeft: "110px",
-              // minWidth: "88%",
-              // right: "0%",
-              padding: "0px",
-              //   height: "100%",
-
-              //   backgroundColor: "rgb(136, 136, 136)",
-            }}
-          >
-            <PageLayout />
+          <Col sm={11} className="middleCol">
+            <PageLayout page={props} />
           </Col>
         </Row>
       </Container>
