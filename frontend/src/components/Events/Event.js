cff792e3265e55944cb7729f6a97a525e4413705
Jonathan Frucht <jonfrucht@gmail.com>
2021-03-28 00:41:13 -0400

    Cleaned up + Styled modal

--- frontend/src/components/Events/Event.js
+++ frontend/src/components/Events/Event.js
@@ -8,68 +8,74 @@ import {
   Modal,
   Row,
 } from "react-bootstrap";
-import "./EventContainer.css";
+import "./Event.css";
 import pupFiller from "../../pictures/pupFiller.jpeg";
 
 const Event = (props) => {
+  // Modal fields
+  const [show, setShow] = useState(false);
+  const modalOpen = () => setShow(true);
+  const modalClose = () => setShow(false);
+  // Event fields
   const [eventName, setName] = useState("");
   const [eventRso, setRso] = useState("");
   const [eventDesc, setDesc] = useState("S");
   const [eventDate, setDate] = useState("MM/DD/YYYY");
   useEffect(() => {
-    console.log("UseEffect");
-    console.log(props);
+    // console.log("UseEffect");
+    // console.log(props);
 
     setName(props.event.name);
-    console.log(props.event.name);
+    // console.log(props.event.name);
     setRso(props.event.rso);
     setDesc(props.event.desc);
     setDate(props.event.date);
   }, []);
 
-  const eventModal = (props) => {
-    <Modal show={true}>
-      <Container style={{ padding: "2rem" }}>
-        <Row>
-          <Image className="modalImage" src={pupFiller} />
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <b>Event Name:{"\xa0"}</b>
-              {eventName}
-            </Row>
-            <Row>
-              <b>Hosted by:{"\xa0"}</b>
-              {eventRso}
-            </Row>
-            <Row>
-              <b>Date:{"\xa0"}</b>
-              {eventDate}
-            </Row>
-            <Row>
-              <b>Description:{"\xa0"}</b>
-              {eventDesc}
-            </Row>
-          </Col>
-          <Col>
-            <Row>
-              <Button> Add to calendar</Button>
-              <Button> Close </Button>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </Modal>;
-  };
-
   return (
-    <Container style={{ backgroundColor: "yellowgreen" }}>
-      <h1>{eventName}</h1>
-      <h4>{eventRso}</h4>
-      <Image className="previewImage" src={pupFiller} />
-      <p style={{ fontSize: "1.3rem" }}>{eventDesc}</p>
-    </Container>
+    <div>
+      <Container onClick={modalOpen} style={{ backgroundColor: "yellowgreen" }}>
+        <h1>{eventName}</h1>
+        <h4>{eventRso}</h4>
+        <Image className="previewImage" src={pupFiller} />
+        <p style={{ fontSize: "1.3rem" }}>{eventDesc}</p>
+      </Container>
+      {/* Event Modal */}
+      <Modal show={show} onHide={modalClose}>
+        <Container className="modalContainer">
+          <Row>
+            <Image className="modalImage" src={pupFiller} />
+          </Row>
+          <Row style={{ margin: ".5rem" }}>
+            <Col>
+              <Row>
+                <b>Event Name:{"\xa0"}</b>
+                {eventName}
+              </Row>
+              <Row>
+                <b>Hosted by:{"\xa0"}</b>
+                {eventRso}
+              </Row>
+              <Row>
+                <b>Date:{"\xa0"}</b>
+                {eventDate}
+              </Row>
+              <Row>
+                <b>Description:{"\xa0"}</b>
+                {eventDesc}
+              </Row>
+            </Col>
+          </Row>
+          <Modal.Footer>
+            <Row>
+              <Button style={{ marginRight: "7.1rem" }}> View Event</Button>
+              <Button style={{ marginRight: "1rem" }}> Add to calendar</Button>
+              <Button onClick={modalClose}> Close </Button>
+            </Row>
+          </Modal.Footer>
+        </Container>
+      </Modal>
+    </div>
   );
 };
 export default Event;
