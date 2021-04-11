3e0cc710c7894b7f932c02fa773bb0a1d6f089df
Jonathan Frucht <jonfrucht@gmail.com>
2021-04-11 19:54:59 -0400

    Added linking for buttons

--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -7,42 +7,44 @@ const SideMenu = (props) => {
   const renderMenu = (
     <div>
       {props.type ? (
-      <Col style={{ padding: 0 }}>
-        <Nav defaultActiveKey="/dashboard" className="flex-column">
-          <Nav.Link href="/dashboard" className="my-nav-link">
-            Home
-          </Nav.Link>
-          <Nav.Link className="my-nav-link">Events</Nav.Link>
-          <Nav.Link className="my-nav-link">Groups</Nav.Link>
-          <Nav.Link href="/my-account" className="my-nav-link">
-            Account
-          </Nav.Link>
-          <Nav.Link href="/admin-profile" className="my-nav-link">Profile</Nav.Link>
-        </Nav>
-      </Col>
-    ) : (
-      <Col style={{ padding: 0 }}>
-        <Nav defaultActiveKey="/Home" className="flex-column">
-          <Nav.Link href="/Home" className="my-nav-link">
-            Home
-          </Nav.Link>
-          <Nav.Link className="my-nav-link">My Events</Nav.Link>
-          <Nav.Link className="my-nav-link">My Groups</Nav.Link>
-          <Nav.Link href="/my-account" className="my-nav-link">
-            My Account
-          </Nav.Link>
-          <Nav.Link className="my-nav-link">My Profile</Nav.Link>
-        </Nav>
-      </Col>
-    )}
+        <Col style={{ padding: 0 }}>
+          <Nav defaultActiveKey="/dashboard" className="flex-column">
+            <Nav.Link href="/dashboard" className="my-nav-link">
+              Home
+            </Nav.Link>
+            <Nav.Link className="my-nav-link">Events</Nav.Link>
+            <Nav.Link className="my-nav-link">Groups</Nav.Link>
+            <Nav.Link href="/my-account" className="my-nav-link">
+              Account
+            </Nav.Link>
+            <Nav.Link href="/admin-profile" className="my-nav-link">
+              Profile
+            </Nav.Link>
+          </Nav>
+        </Col>
+      ) : (
+        <Col style={{ padding: 0 }}>
+          <Nav defaultActiveKey="/Home" className="flex-column">
+            <Nav.Link href="/Home" className="my-nav-link">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/events" className="my-nav-link">
+              Events
+            </Nav.Link>
+            <Nav.Link href="/my-groups" className="my-nav-link">
+              My Groups
+            </Nav.Link>
+            <Nav.Link href="/my-account" className="my-nav-link">
+              My Account
+            </Nav.Link>
+            {/* <Nav.Link className="my-nav-link">My Profile</Nav.Link> */}
+          </Nav>
+        </Col>
+      )}
     </div>
   );
 
-  return (
-    <div>
-      {renderMenu}
-    </div>
-  );
+  return <div>{renderMenu}</div>;
 };
 
 export default SideMenu;
