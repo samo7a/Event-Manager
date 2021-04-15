b3e14bc141207ee96af62575edd4742b89401120
StaceyD22 <stacey.dale2311@gmail.com>
2021-04-15 15:06:49 -0400

    update dashboard content

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MyCalendar from './MyCalendar';
 import SideMenu from './SideMenu';
+import ApproveEvents from './ApproveEvents';
 import './AdminDashboard.css';
 import 'react-calendar/dist/Calendar.css';
 import { getUnequalProps } from '@fullcalendar/react';
@@ -16,7 +17,9 @@ const AdminDashboard = (props) => {
                 <Col xs={1}>
                     <SideMenu type={props.type} />
                 </Col>
-                <Col xs={3}>This will be events that need approval</Col>
+                <Col xs={3}>
+                    <ApproveEvents />
+                </Col>
                 <Col>
                     <MyCalendar />
                 </Col>
