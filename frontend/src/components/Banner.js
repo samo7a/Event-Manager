ab555ab239af1cad24f8d74d058edc4c66163edb
StaceyD22 <stacey.dale2311@gmail.com>
2021-04-15 15:37:05 -0400

    update banner

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -43,7 +43,7 @@ const MyNavBar = (props) => {
 
   useEffect(() => {
     localStorage.setItem('user', JSON.stringify(userInfo));
-    console.log("Printing local storage: " + JSON.parse(localStorage.getItem('user')));
+    console.log("Printing local storage: ", JSON.parse(localStorage.getItem('user')));
   }, [userInfo]);
 
   const doLogin = async (event) => {
