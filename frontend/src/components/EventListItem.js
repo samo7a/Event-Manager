d5b760571ceb015a320d3bc7f01f543534ea0166
StaceyD22 <stacey.dale2311@gmail.com>
2021-04-15 15:59:32 -0400

    update event appearance

--- frontend/src/components/EventListItem.js
+++ frontend/src/components/EventListItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './EventListItem.css';
 
 const EventListItem = (props) => {
 
     return (
         <div className={props.myStyle}>
             <span><strong>{props.name}</strong></span>
-            
+            <div>
+                {props.date}
+                <Button onClick={props.approveEvent}>Approve</Button>
+            </div>
         </div>
-
     );
 };
 
