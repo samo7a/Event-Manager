c860bbb7936583aac6a6eb7c9da82a4ceea0c179
Jonathan Frucht <jonfrucht@gmail.com>
2021-04-17 00:07:06 -0400

    cleanup

--- frontend/src/components/Groups/Group.js
+++ frontend/src/components/Groups/Group.js
@@ -35,20 +35,11 @@ const Group = (props) => {
     // setThumbnail(props.group.groupTN);
   }, []);
 
-  // const activeStatus = {isActive ?
-  // {<span>}"active" : "not active"};
   return (
     <div>
       <Card style={{ maxHeight: "8.12rem" }}>
         <Row id="cardRow" style={{ padding: 0 }}>
           <Col id="ThumbnailIMGCol" xs="auto" style={{ maxHeight: "10%" }}>
-            {/* <Card.Img
-              className="groupThumbnailImage"
-              src={GroupThumbnail}
-              // thumbnail
-              roundedCircle
-              style={{ margin: ".1rem" }}
-            /> */}
             <Image
               src={GroupThumbnail}
               roundedCircle
@@ -57,30 +48,21 @@ const Group = (props) => {
             {/* {console.log(group)} */}
           </Col>
           <Col id="cardInfo" style={{ paddingLeft: ".1rem" }}>
-            {/* <Card> */}
             <Row style={{ marginLeft: "0" }}>
               <h4>{rsoName}</h4>
               <Card.Text style={{ marginLeft: "10rem", marginTop: ".25rem" }}>
-                {/* <h4>RSO NAME</h4> */}
-                {/* This group is{" "} */}
                 {isActive ? (
                   <span style={{ color: "green", fontWeight: "bold" }}>
                     Active
                   </span>
                 ) : (
                   <span style={{ color: "red", fontWeight: "bold" }}>
-                    ot active
+                    Not active
                   </span>
                 )}
               </Card.Text>
             </Row>
-            {/* <Card.Text>
-              <span>Total Members: {totalMembers} </span>
-              <span style={{ marginLeft: "1.5rem" }}>
-                Total Admins: {totalAdmins}
-              </span>
-              <div>Next event: Fun day at the park!</div>
-            </Card.Text> */}
+
             <Card.Text overflow="hidden">
               I go to loud places To search for someone To be quiet with Who
               will take me home You go to loud places To find someone who Will
@@ -91,16 +73,10 @@ const Group = (props) => {
               style={{
                 width: "100%",
                 marginLeft: 0,
-                // display: "flex",
-                // alignItems: "flex-end",
               }}
             >
-              {/* <Card.Link>Click here to view options</Card.Link> */}
               <Card.Link
                 style={{
-                  // marginLeft: "10rem",
-                  // display: "flex",
-                  // alignItems: "inherit",
                   marginRight: "-1rem",
                 }}
                 href="/single-group"
@@ -108,10 +84,7 @@ const Group = (props) => {
                 View Group Page
               </Card.Link>
             </Row>
-            {/* </Card> */}
           </Col>
-
-          {/* <Col></Col> */}
         </Row>
       </Card>
     </div>
