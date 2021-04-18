4aeaaa773b868df5b032c5be0a06781e21f2f701
StaceyD22 <stacey.dale2311@gmail.com>
2021-04-18 19:24:29 -0400

    Make some things prettier

--- frontend/src/components/EventListItem.js
+++ frontend/src/components/EventListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import moment from 'moment';
 import './EventListItem.css';
 
 const EventListItem = (props) => {
@@ -10,7 +11,7 @@ const EventListItem = (props) => {
                 <strong>{props.name}</strong>
             </span>
             <div className="date-div">
-                {props.date.slice(0, 10)}
+            {moment(props.date.slice(0, 10), "YYYY-MM-DD").format("dddd, MMMM Do YYYY")}
             </div>
             <Button onClick={props.approveEvent}>Approve</Button>
         </div>
